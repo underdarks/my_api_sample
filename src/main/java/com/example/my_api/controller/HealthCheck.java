bbb4e7417aec1b4fce0f04def4992f46afe2fccc
seogjaehyeon <rkdrl45617@gmail.com>
2026-03-19 17:05:38 +0900

    feat: 헬스체크 추가

---
+++ src/main/java/com/example/my_api/controller/HealthCheck.java
@@ -0,0 +1,18 @@
+package com.example.my_api.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/health")
+public class HealthCheck {
+
+    @GetMapping
+    public String healthCheck() {
+        return "Server is Working!";
+    }
+
+}
